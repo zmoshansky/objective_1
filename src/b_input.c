990692efa8ab89339e7c55f561bc0f930df6453b
Your Name <you@example.com>
2014-11-22 11:00:46 -0800

    Removed unnecessary files

--- src/b_input.c
+++
@@ -1,5 +0,0 @@
-/*
-* Code to input on a pin
-* create header file b_input.h to contain function declarations
-* pin, port, ???
-*/
--- src/b_output.c
+++
@@ -1,5 +0,0 @@
-/*
-* Code to output on a pin
-* create header file b_output.h to contain function declarations
-* pin, port, ???
-*/
